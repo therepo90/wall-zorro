a37f38c9261b2c21d71101c8f2ce884304736aa0
Tomasz Szepczynski <tomek.szepczynski@swing.dev>
2019-12-12 19:03:32 +0100

    wall

--- src/app/wall/containers/wall/wall.component.ts
+++ src/app/wall/containers/wall/wall.component.ts
@@ -1,14 +1,16 @@
 import {
     ChangeDetectionStrategy,
+    ChangeDetectorRef,
     Component,
     OnDestroy,
     OnInit,
 } from '@angular/core';
 import { WallService } from '../../services/wall.service';
 import { Post } from '../../models/post';
-import { BehaviorSubject, interval } from 'rxjs';
+import { interval } from 'rxjs';
 import { untilDestroyed } from 'ngx-take-until-destroy';
 import { filter } from 'rxjs/operators';
+import { chunk, orderBy } from 'lodash-es';
 
 @Component({
     selector: 'app-wall',
@@ -17,10 +19,13 @@ import { filter } from 'rxjs/operators';
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class WallComponent implements OnInit, OnDestroy {
-    private _visiblePosts$ = new BehaviorSubject<Post[]>([]);
-    public visiblePosts$ = this._visiblePosts$.asObservable();
+    private _visiblePosts: Post[] = [];
+    public columns: Array<Array<Post>> = [[]];
 
-    constructor(private wallService: WallService) {}
+    constructor(
+        private wallService: WallService,
+        private cd: ChangeDetectorRef
+    ) {}
 
     ngOnInit() {
         this.wallService
@@ -31,16 +36,27 @@ export class WallComponent implements OnInit, OnDestroy {
             )
             .subscribe((posts: Array<Post>) => {
                 const allPosts = posts;
+               /* this.columns = chunk(allPosts, 3);
+                this.cd.markForCheck();
+                return;*/
+
                 const subscription = interval(1000)
                     .pipe(untilDestroyed(this))
                     .subscribe(() => {
-                        this._visiblePosts$.next([
-                            ...this._visiblePosts$.getValue(),
+                        const newArray = [
+                            ...this._visiblePosts,
                             allPosts.shift(),
+                        ];
+                        const sortedArray = orderBy(newArray, e => e.id, [
+                            'desc',
                         ]);
+                        this._visiblePosts = sortedArray;
+                        this.columns = chunk(sortedArray, 3);
+                        console.log({sortedArray}, {x: this.columns})
                         if (allPosts.length === 0) {
                             subscription.unsubscribe();
                         }
+                        this.cd.markForCheck();
                     });
             });
     }
